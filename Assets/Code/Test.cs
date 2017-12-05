49748633aacd2e28aa12db3f819e5b623984951c
Owen Bartolf <owen.bartolf@Gmail.com>
2017-12-04 18:31:47 -0800

    Folder structure additions plus GitIgnore!

---
+++ Assets/Code/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+// ^^ Remove unneccessary using directives to speed up compilation.
+
+/// <summary>
+/// This is an example file. This is how all code should be documented.
+/// </summary>
+public class Test : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("This is a test!");	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("Test Test Test.");
+	}
+}
