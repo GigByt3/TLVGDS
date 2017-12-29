6ee1de7aa3fb38eb2544f81c7a13fee3394466b6
GigByt3 <p3n.duke@gmail.com>
2017-12-29 00:44:42 -0800

    Update Random.cs

--- Assets/Code/Random.cs
+++ Assets/Code/Random.cs
@@ -1,3 +1,7 @@
 [SerializableAttribute]
 [ComVisibleAttribute(true)]
 public class Random
+
+///<summary>
+///  This is a "random class" , by calling Random() anywhere in this file or I belive any inherited files, you should have a random number returned, another option is to call Random(int??) to get a random number with the seed of ??. i.e. if you ever call ?? again with this function you will always get the same result (Perlin Noise)
+///</summary>
