287463fcf3bcfca3a0391badd7d30c2a53c29d24
Finlay <p3n.duke@gmail.com>
2018-01-21 17:52:51 -0800

    NetworkManagerClass.cs and CharecterIdentity.cs Classes Done.

--- Assets/Code/CharecterIdentity.cs
+++ Assets/Code/CharecterIdentity.cs
@@ -1,6 +1,11 @@
-public class charecterIdentity(seed) : NetworkBehavior
+public class charecterIdentity : NetworkBehavior
 {
-    Random rnd = new Random(seed);
-    int id = rnd.Next(10000, 100000);
-    int preveived_id = rnd.Next(12);
+    public void createIdentity()
+    {
+        Random rnd = new Random(seed, type);
+        int id = rnd.Next(10000, 100000);
+        if(type == true) {return(id)};
+        int preveived_id = rnd.Next(12);
+        return(preveived_id);    
+    }
 }
