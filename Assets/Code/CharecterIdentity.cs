b0902fdf609ae59e20104b87d4ccb2b15e7287bb
Finlay <p3n.duke@gmail.com>
2018-01-21 16:36:18 -0800

    50% to commpletion Network Manager Hitpoints and Charecter, can wortk on other CP functions later

--- Assets/Code/CharecterIdentity.cs
+++ Assets/Code/CharecterIdentity.cs
@@ -1,6 +1,6 @@
-public class charecterProperties : NetworkBehavior
+public class charecterIdentity(seed) : NetworkBehavior
 {
-    Random rnd = new Random();
+    Random rnd = new Random(seed);
     int id = rnd.Next(10000, 100000);
     int preveived_id = rnd.Next(12);
 }
--- Assets/Code/NetworkManagerClass.cs
+++ Assets/Code/NetworkManagerClass.cs
@@ -1,25 +1,9 @@
-public class CharecterMananager : Network
+public class CharecterMananager : NetworkBehavior
 {
-    public string  myField = string.Empty;
-
-    public MyClass()
+    int[][] playerInformation = new int[][]
+    for (int players = 0; players < 8; players++)
     {
+        playerInformation[0][players] = 100
+        CharecterIdentity id = new CharecterIdentity(players*100);
     }
-
-    public void MyMethod(int parameter1, string parameter2)
-    {
-        Console.WriteLine("First Parameter {0}, second parameter {1}", 
-                                                    parameter1, parameter2);
-    }
-
-    public int MyAutoImplementedProperty { get; set; }
-
-    private int myPropertyVar;
-    
-    public int MyProperty
-    {
-        get { return myPropertyVar; }
-        set { myPropertyVar = value; }
-
-    } 
 }
