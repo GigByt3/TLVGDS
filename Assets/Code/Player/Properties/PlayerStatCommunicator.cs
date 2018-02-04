c5319b841f7b9a18d7d36fc70fcb5d0ef55f2338
Finlay <p3n.duke@gmail.com>
2018-02-03 19:49:15 -0800

    Edits

--- Assets/Code/Player/Properties/PlayerStatCommunicator.cs
+++ Assets/Code/Player/Properties/PlayerStatCommunicator.cs
@@ -5,7 +5,8 @@ public class Player
     int y = rnd(-10, 10);
     int class = rnd(12);
     int precivedId = rnd(4);
-    public event EventHandler Player;
+    public delegate int statsToServer(stats);
+    public event EventHandler Player(stats);
     int[] playerInfo = new int[x, y, NetworkPlayer.ipAddress, 100, class, precivedId];
     /// Structure: [x, y, id, hp, class#, precievedId#]
     /// Class# and precievedId# corespond to classes on a server based array
--- Assets/Code/Utility/CharecterTracker.cs
+++ Assets/Code/Utility/CharecterTracker.cs
@@ -3,17 +3,19 @@ public class CharecterTracker
     int[][] charectersAndProperties = new int[][]
 
     void Update() {
-        Player SendingPlayer = new Player()
-        SendingPlayer.Player += playerInformation
-        bool exists = playerInformation.Any(x=> x.Any(y=> y == playerInformation[2]));
-        if (exists)
-        {
-        int[] result = charectersAndProperties.FirstOrDefault(x=>x.Any(y=>y == playerInformation[2]));    
-        int pos = IndexOf(charectersAndProperties, result)
-        charectersAndProperties[pos] = playerInformation
-        } else {
-        charectersAndProperties[charectersAndProperties.length + 1] = playerInformation
-        }
+        
+
+        // Player SendingPlayer = new Player()
+        // SendingPlayer.Player += playerInformation
+        // bool exists = playerInformation.Any(x=> x.Any(y=> y == playerInformation[2]));
+        // if (exists)
+        // {
+        // int[] result = charectersAndProperties.FirstOrDefault(x=>x.Any(y=>y == playerInformation[2]));    
+        // int pos = IndexOf(charectersAndProperties, result)
+        // charectersAndProperties[pos] = playerInformation
+        // } else {
+        // charectersAndProperties[charectersAndProperties.length + 1] = playerInformation
+        // }
     }
 
 }
