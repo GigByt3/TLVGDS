f2aead521038bf4d8797aa259f93f8a0b5d39014
Finlay <p3n.duke@gmail.com>
2018-02-03 19:48:27 -0800

    Class Commits Test

---
+++ Assets/Code/Player/Properties/PlayerStatCommunicator.cs
@@ -0,0 +1,20 @@
+public class Player 
+{
+    Random rnd = new Random();
+    int x = rnd(-10, 10);
+    int y = rnd(-10, 10);
+    int class = rnd(12);
+    int precivedId = rnd(4);
+    public event EventHandler Player;
+    int[] playerInfo = new int[x, y, NetworkPlayer.ipAddress, 100, class, precivedId];
+    /// Structure: [x, y, id, hp, class#, precievedId#]
+    /// Class# and precievedId# corespond to classes on a server based array
+
+    void Update() {
+        protected virtual void UpdatePlayer(playerInfo)
+        {
+            EventHandler handler = Player;
+            handler(playerInfo);
+        };
+    };
+};
