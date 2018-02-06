488017a9194dbdabb9f5f967b4556daeefa9f046
GigByt3 <p3n.duke@gmail.com>
2018-02-05 16:03:12 -0800

    Create IdentityProperties.cs

---
+++ Assets/Code/Player/Properties/IdentityProperties.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// The Identity property contains logic for keeping track of identity
+/// This property component contains events for external scripts to extend its behaviors.
+/// </summary>
+public class IdentityProperty : NetworkBehaviour
+{
+    #region Fields
+    [SyncVar]
+    public string trueIdentity = SystemInfo.deviceUniqueIdentifier;
+    [SyncVar]
+    public float precievedIdentity = ;
+    #endregion
+
+    #region Events
+    public delegate void _OnPreceivedIdentityChanged(object source; eventArgs args);
+    public event _OnPreceivedIdentityChanged OnPreceivedIdentityChanged;
+    protected vitrual void OnPreceivedIdentityChanged()
+    {
+        if (PreceivedIdentityChanged != null)
+        {
+            PreceivedIdentityChanged(this, [trueIdentity, precievedIdentity])
+        }
+    }
+    #endregion
+}
