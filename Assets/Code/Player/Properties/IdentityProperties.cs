07b9ce54fb834f5a2bebfc7726aff48ac8c190d0
jimmyjam69 <jimmyjam69@googlemail.com>
2018-02-11 13:47:07 -0800

    Asset examples
    Added in Sci Fi Tiles and Knight anim set

--- Assets/Code/Player/Properties/IdentityProperties.cs
+++ Assets/Code/Player/Properties/IdentityProperties.cs
@@ -7,17 +7,17 @@ using UnityEngine.Networking;
 /// The Identity property contains logic for keeping track of identity
 /// This property component contains events for external scripts to extend its behaviors.
 /// </summary>
-public class IdentityProperty : NetworkBehaviour
+/*public class IdentityProperty : NetworkBehaviour
 {
     #region Fields
     [SyncVar]
     public string trueIdentity = SystemInfo.deviceUniqueIdentifier;
     [SyncVar]
-    public float precievedIdentity = ;
+    //public float precievedIdentity = ;
     #endregion
 
     #region Events
-    public delegate void _OnPreceivedIdentityChanged(object source; eventArgs args);
+    public delegate void _OnPreceivedIdentityChanged(object source, eventArgs args);
     public event _OnPreceivedIdentityChanged OnPreceivedIdentityChanged;
     protected vitrual void OnPreceivedIdentityChanged()
     {
@@ -28,3 +28,4 @@ public class IdentityProperty : NetworkBehaviour
     }
     #endregion
 }
+*/
