98e1402d35e1c3eef72615e99db2d671d2d0ff2a
Akira J. Schneider <33035432+Akira-Schneider@users.noreply.github.com>
2018-02-11 17:40:57 -0800

    Ivory attack script
    might compile

--- Assets/Code/Player/Attacks/IvoryAttack.cs
+++ Assets/Code/Player/Attacks/IvoryAttack.cs
@@ -1,7 +1,24 @@
-public class IvoryAttack : BasePlayerAtack
+public class IvoryAttack : BasePlayerAttack
 {
-    damagePerHit = 10f;
-    range = 10f;
-    currentAttackCooldown = 3;
-    maxAttackCooldown = -1;
+
+    public override ServerAttackMethod(Vector3 aimingVector)
+    { 
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, aimingVector, range);
+        
+        int poison = (int)EffectType.HealthPerSecond 2
+        // Hit logic
+        if (hit.transform != null)
+        {
+            // How to damage players from the server but using effects on contact: 
+            // 1. Search for a PlayerEffectsManager:
+            PlayerEffectsManager playerEffectsManager = hit.transform.GetComponent<PlayerEffectsManager.cs>();
+
+            // 2. If one exists, just damage it. Simple!
+            if(playerEffectsManager != null)
+            {
+            
+                  playerEffectsManager.RpcAddEffect(PlayerEffect poison)
+            }
+       }
+    }
 }
